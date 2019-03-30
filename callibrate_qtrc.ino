fefb583857134f84aa960443f1dce36c10ea0362
atom-hr <49087046+atom-hr@users.noreply.github.com>
2019-03-30 12:18:55 +0600

    the basic instructions

---
+++ callibrate_qtrc.ino
@@ -0,0 +1,29 @@
+int calibration()
+{
+  indicator_off;
+  for(int i=0;i<100;i++)
+  {
+    qtrrc.calibrate();
+    blink(20);
+  }
+  delay(2000);
+
+  if(true)
+  {
+    for (int i = 0; i < NUM_SENSORS; i++)
+    {
+      Serial.print(qtrrc.calibratedMinimumOn[i]);
+      Serial.print(' ');
+    }
+    Serial.println();
+
+    for (int i = 0; i < NUM_SENSORS; i++)
+    {
+      Serial.print(qtrrc.calibratedMaximumOn[i]);
+      Serial.print(' ');
+    }
+    Serial.println();
+    Serial.println();
+  }
+  indicator_on;
+}
